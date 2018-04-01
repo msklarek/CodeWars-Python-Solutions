134ac1429fdde4ba1c3b1df061a13b1c4d9cac43
Malgorzata Sklarek <msklarek@gmail.com>
2018-04-01 15:38:10 -0700

    codewars
    comments were added

--- katas/kyu_6/Your_order_please.py
+++ katas/kyu_6/Your_order_please.py
@@ -3,10 +3,15 @@ def order(sentence):
     d = {}
     for i in a:
         for k in i:
+            # building dictonary where key is a word and value
+            # is a number of position in a sentence
             if k.isdigit():
                 d[i] = k
+    # sorting dictory by values
     b = sorted([(value, key) for (key, value) in d.items()])
     result = ''
+    # building a sentece made by keys and position depends of a value
     for j in b:
         result = result + ' ' + j[1]
     return result[1:]
+
